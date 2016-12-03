f6c89939e46a9c59d4898320c717c5681dc3eb1f
Aaron Stannard <aaron@petabridge.com>
2016-12-02 17:51:26 -0800

    can install JAVA_HOME

--- installers/1.6.2/install-spark1.6.2.ps1
+++ installers/1.6.2/install-spark1.6.2.ps1
@@ -17,11 +17,19 @@ $currentUser = New-Object Security.Principal.WindowsPrincipal( [Security.Princip
     else{
           (Get-Host).UI.RawUI.Backgroundcolor="Black"
           Clear-Host
-          Write-Host "Starting Installation of Spark 1.6.2 onto your machine.'n"
-          Write-Host "This script does not support weird stuff like corporate firewalls and proxies.'n"
-          Write-Host "This may take a few minutes.'n"
+          Write-Host "Starting Installation of Spark 1.6.2 onto your machine."
+          Write-Host "This script does not support weird stuff like corporate firewalls and proxies."
+          Write-Host "This may take a few minutes."
     }
 }
+
+# Need to be able to reload the PATH of the PowerShell session after any Chocolatey installations
+function ReloadPath
+{
+    $env:Path = [System.Environment]::GetEnvironmentVariable("Path","Machine") + ";" + [System.Environment]::GetEnvironmentVariable("Path","User")
+}
+ReloadPath
+
 
 # Time to check environment variables. If there's a previous installation of Spark, Mobius, JDK, or Hadoop we should
 # stop and ask the end-user for permission to reset each one individually.
@@ -38,10 +46,19 @@ $hadoopHome = [Environment]::GetEnvironmentVariable($hadoopHomeVariableName)
 $mobiusHomeVariableName = "SPARKCLR_HOME"
 $mobiusHome = [Environment]::GetEnvironmentVariable($mobiusHome)
 
-# Need to be able to reload the PATH of the PowerShell session after any Chocolatey installations
-function ReloadPath
-{
-    $env:Path = [System.Environment]::GetEnvironmentVariable("Path","Machine") + ";" + [System.Environment]::GetEnvironmentVariable("Path","User")
+
+
+# Uses the "where" command in batch to find a binary that matches a given command name
+# Useful for finding items that have been installed to the PATH, but don't have an
+# Environment variable set, such as JAVA_HOME
+function FindCommandOnPath([string]$p){
+    Try{
+        return (cmd.exe /c where $p)
+    }
+    Catch{
+        Write-Host "Couldn't find ($p) in PATH."
+        return $null
+    }
 }
 
 # Downloads and installs Chocolatey, if necessary.
@@ -49,16 +66,22 @@ function ReloadPath
 function GetOrInstallChoco
 {
     # Check to see if Chocolatey is installed
-    Write-Host "Checking for Chocolatey installation...'n"
-    $chocInstallVariableName = "ChocolateyInstall"
-    $chocoPath = [Environment]::GetEnvironmentVariable($chocInstallVariableName)
-    if ($chocoPath -eq $null -or $chocoPath -eq '') { 
-        Write-Host "Chocolatey (https://chocolatey.org/) not detected on this system. Installing...'n"
-        (iex ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'))) | Out-Host
-        ReloadPath
+    Write-Host "Checking for Chocolatey installation..."
+    Try{
+        $chocInstallVariableName = "ChocolateyInstall"
+        $chocoPath = [Environment]::GetEnvironmentVariable($chocInstallVariableName)
+        if ($chocoPath -eq $null -or $chocoPath -eq '') { 
+            Write-Host "Chocolatey (https://chocolatey.org/) not detected on this system. Installing..."
+            (iex ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'))) | Out-Host
+            ReloadPath
+        }
+        else{
+            Write-Host "Found Chocolatey at ($chocoPath)"
+        }
     }
-    else{
-        Write-Host "Found Chocolatey at ($chocoPath)"
+    Catch{
+        Write-Host "Failed to install Chocolatey. Reason: ($_.Exception.Message)."
+        Exit
     }
     return $true
 }
@@ -69,11 +92,11 @@ function InstallJdk
 {
     GetOrInstallChoco
 
-    Write-Host "Executing choco install jdk8 -y 'n"
+    Write-Host "Executing choco install jdk8 -y "
     choco install jdk8 -y | Out-Host
     ReloadPath
-    $rValue = where "javac"
-    Write-Host "Installation finished. Installed JDK to ($rValue)'n"
+    $rValue = FindCommandOnPath("javac")
+    Write-Host "Installation finished. Installed JDK to ($rValue)"
     return $rValue
 }
 
@@ -81,24 +104,26 @@ function InstallJdk
 
 
 
-Write-Host "'n'n------------------------ JDK PREREQUISITES ------------------------'n'n"
-Write-Host "Checking for JDK 8.0 installation...'n"
+Write-Host "------------------------ JDK PREREQUISITES ------------------------"
+Write-Host "Checking for JDK 8.0 installation..."
 if($javaHome -eq $null -or $javaHome -eq ''){
-    Write-Host "JAVA_HOME environment not detected on this system.'n"
-    Write-Host "Scanning file system for JDK8 installation.'n"
-    $javaHome = where "javac"
-    if($javaHome -eq $null -or $javaHome -eq '' -or $javaHome -contains "Could not find files"){
-        Write-Host "Unable to find JDK installation on this system'n"
-        Write-Host "Beginning JDK 8.0 installation...'n"
-        $javaHome = InstallJdk
+    Write-Host "JAVA_HOME environment not detected on this system."
+    Write-Host "Scanning file system for JDK8 installation."
 
-        [Environment]::SetEnvironmentVariable($javaHomeVariableName, $javaHome, 'machine')
-        Write-Host "Set ($javaHomeVariableName) to ($javaHome)'n"
-        
-    }
-    else{
-        Write-Host "Found JDK at ($javaHome)"
-        Write-Host "Checking Java Versiion"
-    }   
+    $javaHome = FindCommandOnPath("javac")
+
     
+    if($javaHome -eq $null -or $javaHome -eq '' -or $javaHome -contains "Could not find files"){
+        Write-Host "Unable to find JDK installation on this system"
+        Write-Host "Beginning JDK 8.0 installation..."
+        $javaHome = InstallJdk
+    }
+
+    [Environment]::SetEnvironmentVariable($javaHomeVariableName, $javaHome, 'machine')
+    Write-Host "Set ($javaHomeVariableName) to ($javaHome)"
 }
+
+Write-Host "Found JDK at ($javaHome)"
+Write-Host "Checking Java Version"
+
+$jvmVersion = cmd.exe /c "javac -version"
