c938760c4f29261658e70f4871de8531338477ad
Aaron Stannard <aaron@petabridge.com>
2016-12-02 18:32:10 -0800

    capturing java version

--- installers/1.6.2/install-spark1.6.2.ps1
+++ installers/1.6.2/install-spark1.6.2.ps1
@@ -2,6 +2,8 @@
 # Copyright (c) Petabridge, LLC
 # Licensed under the Apache 2.0 license. See LICENSE file in the project root for full license information.
 #
+$sparkVersion = "1.6.2"
+$versionRegex = [regex]"\b(\d+\.)?(\d+\.)?(\d+\.)\b" # Regex for version numbers
 
 # First, need to make sure we're running in adminstrator mode or all hell will break loose
 $currentUser = New-Object Security.Principal.WindowsPrincipal( [Security.Principal.WindowsIdentity]::GetCurrent() )
@@ -17,9 +19,9 @@ $currentUser = New-Object Security.Principal.WindowsPrincipal( [Security.Princip
     else{
           (Get-Host).UI.RawUI.Backgroundcolor="Black"
           Clear-Host
-          Write-Host "Starting Installation of Spark 1.6.2 onto your machine."
-          Write-Host "This script does not support weird stuff like corporate firewalls and proxies."
-          Write-Host "This may take a few minutes."
+          Write-Host "Starting Installation of Spark ($sparkVersion) onto your machine.`n"
+          Write-Host "This script does not support weird stuff like corporate firewalls and proxies.`n"
+          Write-Host "This may take a few minutes.`n"
     }
 }
 
@@ -34,7 +36,9 @@ ReloadPath
 # Time to check environment variables. If there's a previous installation of Spark, Mobius, JDK, or Hadoop we should
 # stop and ask the end-user for permission to reset each one individually.
 
+
 $javaHomeVariableName = "JAVA_HOME"
+$minSupportedJdkVersion = "1.8"
 $javaHome = [Environment]::GetEnvironmentVariable($javaHomeVariableName)
 
 $sparkHomeVariableName = "SPARK_HOME"
@@ -66,12 +70,12 @@ function FindCommandOnPath([string]$p){
 function GetOrInstallChoco
 {
     # Check to see if Chocolatey is installed
-    Write-Host "Checking for Chocolatey installation..."
+    Write-Host "Checking for Chocolatey installation...`n"
     Try{
         $chocInstallVariableName = "ChocolateyInstall"
         $chocoPath = [Environment]::GetEnvironmentVariable($chocInstallVariableName)
         if ($chocoPath -eq $null -or $chocoPath -eq '') { 
-            Write-Host "Chocolatey (https://chocolatey.org/) not detected on this system. Installing..."
+            Write-Host "Chocolatey (https://chocolatey.org/) not detected on this system. Installing...`n"
             (iex ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'))) | Out-Host
             ReloadPath
         }
@@ -80,7 +84,7 @@ function GetOrInstallChoco
         }
     }
     Catch{
-        Write-Host "Failed to install Chocolatey. Reason: ($_.Exception.Message)."
+        Write-Host "Failed to install Chocolatey. Reason: ($_.Exception.Message).`n"
         Exit
     }
     return $true
@@ -92,7 +96,7 @@ function InstallJdk
 {
     GetOrInstallChoco
 
-    Write-Host "Executing choco install jdk8 -y "
+    Write-Host "Executing choco install jdk8 -y `n"
     choco install jdk8 -y | Out-Host
     ReloadPath
     $rValue = FindCommandOnPath("javac")
@@ -104,7 +108,7 @@ function InstallJdk
 
 
 
-Write-Host "------------------------ JDK PREREQUISITES ------------------------"
+Write-Host "`n`n------------------------ JDK PREREQUISITES ------------------------`n`n"
 Write-Host "Checking for JDK 8.0 installation..."
 if($javaHome -eq $null -or $javaHome -eq ''){
     Write-Host "JAVA_HOME environment not detected on this system."
@@ -113,9 +117,9 @@ if($javaHome -eq $null -or $javaHome -eq ''){
     $javaHome = FindCommandOnPath("javac")
 
     
-    if($javaHome -eq $null -or $javaHome -eq '' -or $javaHome -contains "Could not find files"){
-        Write-Host "Unable to find JDK installation on this system"
-        Write-Host "Beginning JDK 8.0 installation..."
+    if($javaHome -eq $null -or $javaHome -eq '' -or $javaHome -contains "Could not find files.`n"){
+        Write-Host "Unable to find JDK installation on this system.`n"
+        Write-Host "Beginning JDK 8.0 installation...`n"
         $javaHome = InstallJdk
     }
 
@@ -123,7 +127,13 @@ if($javaHome -eq $null -or $javaHome -eq ''){
     Write-Host "Set ($javaHomeVariableName) to ($javaHome)"
 }
 
-Write-Host "Found JDK at ($javaHome)"
-Write-Host "Checking Java Version"
+Write-Host "Found JDK at ($javaHome)`n"
+Write-Host "Checking Java Version`n"
 
-$jvmVersion = cmd.exe /c "javac -version"
+# For some reason, Java write to STDERR insted of STDOUT, so we have to redirect
+$jvmVersion = (cmd.exe /c "javac -version" 2>&1) | Out-String
+$jvmVersion = $versionRegex.Match($jvmVersion).Groups[0].Value
+Write-Host "Found JDK version ($jvmVersion).`n"
+if($jvmVersion -ccontains $minSupportedJdkVersion){
+    Write-Host "This version of JDK is compatible with Spark ($sparkVerison).'n"
+}
