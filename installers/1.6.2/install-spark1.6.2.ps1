74ee1136a8a772804c504c58e584780c7acea225
Aaron Stannard <aaron@petabridge.com>
2016-12-02 19:16:01 -0800

    working on hadoop installation

--- installers/1.6.2/install-spark1.6.2.ps1
+++ installers/1.6.2/install-spark1.6.2.ps1
@@ -2,8 +2,16 @@
 # Copyright (c) Petabridge, LLC
 # Licensed under the Apache 2.0 license. See LICENSE file in the project root for full license information.
 #
+Param(
+    [string]$hadoopInstallFolder = "C:\Hadoop",
+    [string]$sparkInstallFolder = "C:\Spark",
+    [string]$mobiusInstallFolder = "C:\Mobius",
+    [string]$apacheArchiveServer = "archive.apache.org"
+)
+
+$hadoopVersion = "2.6.0"
 $sparkVersion = "1.6.2"
-$versionRegex = [regex]"\b(\d+\.)?(\d+\.)?(\d+\.)\b" # Regex for version numbers
+$versionRegex = [regex]"\b(?:(\d+)\.)?(?:(\d+)\.)?(\*|\d+)\b" # Regex for version numbers
 
 # First, need to make sure we're running in adminstrator mode or all hell will break loose
 $currentUser = New-Object Security.Principal.WindowsPrincipal( [Security.Principal.WindowsIdentity]::GetCurrent() )
@@ -31,6 +39,11 @@ function ReloadPath
     $env:Path = [System.Environment]::GetEnvironmentVariable("Path","Machine") + ";" + [System.Environment]::GetEnvironmentVariable("Path","User")
 }
 ReloadPath
+
+# Create tools directory
+# Going to need it for our un-taring utility primarily
+$scriptDir = Get-ScriptDirectory
+$toolsDir = "$scriptDir\tools"
 
 
 # Time to check environment variables. If there's a previous installation of Spark, Mobius, JDK, or Hadoop we should
@@ -104,6 +117,119 @@ function InstallJdk
     return $rValue
 }
 
+# This method was developed by the Microsoft team working on the Mobius project.
+# See the original source here: https://github.com/Microsoft/Mobius/blob/6e2b820524c8184b19d2650094480c7c3ae0229c/build/localmode/downloadtools.ps1
+function Download-File($url, $output)
+{
+    $output = [System.IO.Path]::GetFullPath($output)
+    if (test-path $output)
+    {
+        if ((Get-Item $output).Length -gt 0)
+        {
+            Write-Output "[Download-File] $output exists. No need to download."
+            return
+        }
+        else
+        {
+            Write-Output "[Download-File] [WARNING] $output exists but is empty. We need to download a new copy of the file."
+            Remove-Item $output
+        }
+    }
+
+    $start_time = Get-Date
+    $wc = New-Object System.Net.WebClient
+    Write-Output "[Download-File] Start downloading $url to $output ..."
+    $Global:downloadComplete = $false
+    Register-ObjectEvent -InputObject $wc -EventName DownloadFileCompleted `
+        -SourceIdentifier Web.DownloadFileCompleted -Action {
+        $Global:downloadComplete = $True
+    }
+    Register-ObjectEvent -InputObject $wc  -EventName DownloadProgressChanged `
+        -SourceIdentifier Web.DownloadProgressChanged -Action {
+        $Global:Data = $event
+    }
+    
+    $tmpOutput = $output + ".tmp.download"
+    if (test-path $tmpOutput) {
+        Remove-Item $tmpOutput
+    }
+    
+    $wc.DownloadFileAsync($url, $tmpOutput)
+    While (!($Global:downloadComplete)) {
+        $percent = $Global:Data.SourceArgs.ProgressPercentage
+        $totalBytes = $Global:Data.SourceArgs.TotalBytesToReceive
+        $receivedBytes = $Global:Data.SourceArgs.BytesReceived
+        If ($percent -ne $null) {
+            Write-Progress -Activity ("Downloading file to {0} from {1}" -f $output,$url) -Status ("{0} bytes \ {1} bytes" -f $receivedBytes,$totalBytes)  -PercentComplete $percent
+        }
+    }
+    
+    Rename-Item $tmpOutput -NewName $output
+    Write-Progress -Activity ("Downloading file to {0} from {1}" -f $output, $url) -Status ("{0} bytes \ {1} bytes" -f $receivedBytes,$totalBytes)  -Completed
+    Unregister-Event -SourceIdentifier Web.DownloadFileCompleted
+    Unregister-Event -SourceIdentifier Web.DownloadProgressChanged
+    $duration = $(Get-Date).Subtract($start_time)
+    if ($duration.Seconds -lt 2)
+    {
+        $mills = $duration.MilliSeconds
+        $howlong = "$mills milliseconds"
+    }
+    else
+    {
+        $seconds = $duration.Seconds
+        $howlong = "$seconds seconds"
+    }
+
+    Write-Output "[downloadtools.Download-File] Download completed. Time taken: $howlong"
+    
+    if ( !(test-path $output) -or (Get-Item $output).Length -eq 0)
+    {
+        throw [System.IO.FileNotFoundException] "Failed to download file $output from $url"
+    }
+}
+
+# This method was developed by the Microsoft team working on the Mobius project.
+# See the original source here: https://github.com/Microsoft/Mobius/blob/6e2b820524c8184b19d2650094480c7c3ae0229c/build/localmode/downloadtools.ps1
+function Unzip-File($zipFile, $targetDir)
+{
+    if (!(test-path $zipFile))
+    {
+        Write-Output "[downloadtools.Unzip-File] WARNING!!! $zipFile does not exist. Abort."
+        return
+    }
+
+    if (!(test-path $targetDir))
+    {
+        Write-Output "[downloadtools.Unzip-File] $targetDir does not exist. Creating ..."
+        New-Item -ItemType Directory -Force -Path $targetDir | Out-Null
+        Write-Output "[downloadtools.Unzip-File] Created $targetDir."
+    }
+
+    $start_time = Get-Date
+    Write-Output "[downloadtools.Unzip-File] Extracting $zipFile to $targetDir ..."
+    $entries = [IO.Compression.ZipFile]::OpenRead($zipFile).Entries
+    $entries | 
+        %{
+            #compose some target path
+            $targetpath = join-path "$targetDir" $_.FullName
+            #extract the file (and overwrite)
+            [IO.Compression.ZipFileExtensions]::ExtractToFile($_, $targetpath, $true)
+        }
+    
+    $duration = $(Get-Date).Subtract($start_time)
+    if ($duration.Seconds -lt 2)
+    {
+        $mills = $duration.MilliSeconds
+        $howlong = "$mills milliseconds"
+    }
+    else
+    {
+        $seconds = $duration.Seconds
+        $howlong = "$seconds seconds"
+    }
+
+    Write-Output "[downloadtools.Unzip-File] Extraction completed. Time taken: $howlong"
+}
 
 
 
@@ -111,7 +237,7 @@ function InstallJdk
 Write-Host "`n`n------------------------ JDK PREREQUISITES ------------------------`n`n"
 Write-Host "Checking for JDK 8.0 installation..."
 if($javaHome -eq $null -or $javaHome -eq ''){
-    Write-Host "JAVA_HOME environment not detected on this system."
+    Write-Host "$javaHomeVariableName environment not detected on this system."
     Write-Host "Scanning file system for JDK8 installation."
 
     $javaHome = FindCommandOnPath("javac")
@@ -135,5 +261,16 @@ $jvmVersion = (cmd.exe /c "javac -version" 2>&1) | Out-String
 $jvmVersion = $versionRegex.Match($jvmVersion).Groups[0].Value
 Write-Host "Found JDK version ($jvmVersion).`n"
 if($jvmVersion -ccontains $minSupportedJdkVersion){
-    Write-Host "This version of JDK is compatible with Spark ($sparkVerison).'n"
+    Write-Host "This version of JDK is compatible with Spark ($sparkVersion).`n"
+}
+else{
+    Write-Host "This version of the JDK is not compatible with ($sparkVersion).`n"
+    Write-Host "Please upgrade manually to at least JDK ($minSupportedJdkVersion) or uninstall it and re-run this script.`n"
+    Exit
+}
+
+Write-Host "`n`n------------------------ HADOOP PREREQUISITES ------------------------`n`n"
+Write-Host "Checking for Hadoop installation..."
+if($hadoopHome -eq $null -or $hadoopHome -eq ''){
+    Write-Host "$hadoopHomeVariableName environment not detected on this system."
 }
