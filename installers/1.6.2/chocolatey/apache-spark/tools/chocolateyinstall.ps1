48344c326a33a2ceb707cf6484a6c91b2fd7012a
Aaron Stannard <aaron@petabridge.com>
2017-03-28 10:40:55 -0700

    updated package metadata

--- installers/1.6.2/chocolatey/apache-spark/tools/chocolateyinstall.ps1
+++ installers/1.6.2/chocolatey/apache-spark/tools/chocolateyinstall.ps1
@@ -1,6 +1,6 @@
 ﻿$ErrorActionPreference = 'Stop'; # stop on all errors
 
-$packageName= 'spark-installer-windows' # arbitrary name for the package, used in messages
+$packageName= 'apache-spark' # arbitrary name for the package, used in messages
 $toolsDir   = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
 $url        = 'https://raw.githubusercontent.com/petabridge/spark-installer-windows/master/installers/1.6.2/install-spark1.6.2.ps1'
 $fileLocation = Join-Path $toolsDir 'install-spark1.6.2.ps1'
