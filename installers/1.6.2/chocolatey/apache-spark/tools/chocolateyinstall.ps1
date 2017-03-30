a93b6a3d4afea2bc6bdd63d1c7afb149c69ec166
Nick Chamberlain <nick.chamberlain.jr@gmail.com>
2017-03-30 16:26:20 +0000

    revise chocolateyinstall.ps1 to not use CHocolateyProcessAsAdmin

--- installers/1.6.2/chocolatey/apache-spark/tools/chocolateyinstall.ps1
+++ installers/1.6.2/chocolatey/apache-spark/tools/chocolateyinstall.ps1
@@ -2,6 +2,7 @@
 
 $packageName= 'apache-spark' # arbitrary name for the package, used in messages
 $toolsDir   = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
-$fileLocation = Join-Path $toolsDir 'install-spark1.6.2.ps1'
+$psFile = Join-Path $toolsDir 'install-spark1.6.2.ps1'
 
-Start-ChocolateyProcessAsAdmin "& `'$fileLocation`'"
+Install-ChocolateyPowershellCommand -PackageName 'install-spark1.6.2.powershell' -PSFileFullPath $psFile
+Invoke-Expression -Command 'install-spark1.6.2'
